7a8a9112b0acb04cde077f3ffb80310f351e02bf
tipenko <tipenko@gmail.com>
2021-02-10 15:21:03 +0300

    add hover highlight to dropzones

--- src/board/Column/Column.tsx
+++ src/board/Column/Column.tsx
@@ -5,25 +5,30 @@ import { moveCard } from '../actionCreators';
 import { ItemTypes } from '../../DNDConstants';
 import { useDrop } from 'react-dnd';
 
-const getDropZone = (targetName, moveCard) => ({index}) => {
-  const [, drop] = useDrop({
+const getDropZone = (targetName, moveCard) => ({ index }) => {
+  const [{ isOver }, drop] = useDrop({
     accept: ItemTypes.CARD,
-    drop: ({card, source})=> moveCard(card, source, targetName, index)
+    drop: ({ card, source }) => moveCard(card, source, targetName, index),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
   });
 
-  return <div className="column-drop-zone" ref={drop}/>
-}
+  return (
+    <div className={`column-drop-zone ${isOver ? 'hovered' : ''}`} ref={drop} />
+  );
+};
 
 const ColumnComponent = ({ name, cards, moveCard }) => {
- const DropZone = getDropZone(name, moveCard);
+  const DropZone = getDropZone(name, moveCard);
   return (
     <div className="kanban-board-column">
       <h2>{name}</h2>
-      <DropZone index={0}/>
-      {cards.map((card, index:number) => (
+      <DropZone index={0} />
+      {cards.map((card, index: number) => (
         <>
-        <Card cardObject={card} source={name}/>
-        <DropZone index={index+1}/>
+          <Card cardObject={card} source={name} />
+          <DropZone index={index + 1} />
         </>
       ))}
     </div>
@@ -32,6 +37,6 @@ const ColumnComponent = ({ name, cards, moveCard }) => {
 
 const mapDispatchToProps = {
   moveCard,
-}
+};
 
 export const Column = connect(null, mapDispatchToProps)(ColumnComponent);
