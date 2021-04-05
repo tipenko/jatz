7a1fd06ad4201f7bdcf58e5c6515e86a811ac234
tipenko <tipenko@gmail.com>
2021-04-05 23:13:53 +0300

    use table from material-ui to show events

--- src/LogEventTimeline/index.tsx
+++ src/LogEventTimeline/index.tsx
@@ -1,60 +1,53 @@
 import React, { Fragment, useMemo, useState, useCallback } from 'react';
-import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineOppositeContent from '@material-ui/lab/TimelineContent';
 
-import TimelineDot from '@material-ui/lab/TimelineDot';
-import { makeStyles } from '@material-ui/core/styles';
 import map from 'lodash/map';
 import sortBy from 'lodash/sortBy';
 import dateFormat from 'dateformat';
 
-const useStylesForMuiTimeline = makeStyles(
-  {
-    //missingOppositeContent: {
-    //  '&::before': { content: 'none' },
-    // },
-    root: {
-      minHeight: '40px',
-    },
-  },
-  { name: 'MuiTimelineItem' }
-);
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const format = (time) => dateFormat(time, 'ddd HH:MM');
 
 const MyTimelineItem = ({ logEvent, isLast }) => {
   return (
-    <TimelineItem
-      key={logEvent.time}
-      title={logEvent.getLongText() + ' ' +format(logEvent.time)}
-    >
-      <TimelineContent>{logEvent.getShortText()}</TimelineContent>
-      <TimelineSeparator>
-        <TimelineDot />
-        {isLast ? null : <TimelineConnector />}
-      </TimelineSeparator>
-      <TimelineOppositeContent>{format(logEvent.time)}</TimelineOppositeContent>
-    </TimelineItem>
+    <Fragment key={logEvent.time}>
+      {format(logEvent.time)}
+      {logEvent.getShortText()}
+    </Fragment>
   );
 };
 
 const LogEventTimeline = ({ logEvents }) => {
-  useStylesForMuiTimeline();
   const sortedLogEvents = useMemo(() => sortBy(logEvents, ['time']), [
     logEvents,
   ]);
 
-  return (
-    <Timeline align="right">
-      {map(sortedLogEvents, (logEvent, index, { length }) => (
-        <MyTimelineItem isLast={index === length - 1} logEvent={logEvent} />
-      ))}
-    </Timeline>
-  );
+  return <TableContainer component={Paper}>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Time</TableCell>
+            <TableCell align="right">Type</TableCell>
+            <TableCell align="right"> Description</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sortedLogEvents.map((logEvent) => (
+            <TableRow key={logEvent.time}>
+              <TableCell align="right">{format(logEvent.time)}</TableCell>
+              <TableCell align="right">{logEvent.type}</TableCell>
+              <TableCell align="right">{logEvent.getLongText()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
 };
 
 export default LogEventTimeline;
