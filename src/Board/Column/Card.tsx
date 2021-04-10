bf2a7c4b01bf240ed239be26fd8741596e99c9fb
tipenko <tipenko@gmail.com>
2021-04-10 21:44:07 +0300

    simplify card appearance

--- src/Board/Column/Card.tsx
+++ src/Board/Column/Card.tsx
@@ -1,35 +1,18 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React from 'react';
 import { useDrag } from 'react-dnd';
 import { Link as RouterLink } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
-
 import { ItemTypes } from '../../DNDConstants';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-
-const usePersistentHoverCallbacks = () => {
-  const [isHover, setHover] = useState(false);
-  const getListener = useCallback((value) => (event) => setHover(value), [
-    setHover,
-  ]);
-
-  const onHover = useMemo(() => getListener(true), [getListener]);
-  const onLeave = useMemo(() => getListener(false), [getListener]);
-
-  return { onHover, onLeave, isHover };
-};
-
-const useStyles = makeStyles({
-  root: {
-    opacity: (isHover) => (isHover ? '1' : '0'),
-    transitionDuration: '0.2s',
-    transitionProperty: 'opacity',
+const useStyles = makeStyles(() => ({
+  routerLink: {
+    textDecoration: 'none',
+    color: 'black'
   },
-});
+}));
 
 const JCard = ({ cardObject, source }) => {
   const [{ isDragging }, drag] = useDrag({
@@ -43,33 +26,24 @@ const JCard = ({ cardObject, source }) => {
     }),
   });
 
-  const { onHover, onLeave, isHover } = usePersistentHoverCallbacks();
-
-  const styles = useStyles(isHover);
+  const styles = useStyles();
 
   return (
     <span
       ref={drag}
       style={{ opacity: isDragging ? 0.5 : 1 }}
-      className="kanban-board-column-card"
     >
-      <Card onMouseOver={onHover} onMouseLeave={onLeave}>
+      <Card>
         <CardContent>
-          <Typography variant="body2" component="p">
+          <Typography
+            variant="body2"
+            component={RouterLink}
+            to={`kanban/cardDetails/${cardObject.uid}`}
+            className={styles.routerLink}
+          >
             {cardObject.content}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button
-            size="small"
-            color="primary"
-            component={RouterLink}
-            to={`kanban/cardDetails/${cardObject.uid}`}
-            classes={styles}
-          >
-            EDIT
-          </Button>
-        </CardActions>
       </Card>
     </span>
   );
